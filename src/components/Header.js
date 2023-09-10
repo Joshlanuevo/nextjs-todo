880a3b1efd99760309ff873a80f0618fedd67590
Joshlanuevo <lanuevo68@gmail.com>
2023-09-10 12:11:51 +0800

    Second Commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return ( 
+        <div className="header-nav p-5 bg-[#242526] text-white">
+            <h1 className="text-lg">Todo App</h1>
+        </div>
+     );
+}
+ 
+export default Header;
